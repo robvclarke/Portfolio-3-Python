d58c8d6a7e6b1b0b66ff9797565e02aaa66c422c
robvclarke <robvclarke@gmail.com>
2021-11-13 22:18:03 +0000

    Add futher spacing and try make font green again

--- run.py
+++ run.py
@@ -8,19 +8,19 @@ while True:
 
         answer = input("Would you like to follow it left or right?(left/right)\n").lower().strip()
         if answer == "left":
-            answer = input("You encounter an agent, would you like to run or attack?")
+            answer = input("You encounter an agent, would you like to run or attack?\n")
 
             if answer == "attack":
                 print("that was not the greatest idea, you lost")
             else:
                 print("good choice you made it away")
 
-                answer = input("Another agent appears in the distance and fires a gun at you? Do you try and dodge it or run? (dodge/run)").lower().strip()
+                answer = input("Another agent appears in the distance and fires a gun at you? Do you try and dodge it or run? (dodge/run)\n").lower().strip()
 
                 if answer == "run":
-                    print("You can't outrun a bullet... Game over")
+                    print("You can't outrun a bullet... Game over\n")
                 else:
-                    print("You dodged the bullet and brought peace between Man and Machine. You won the game you legend!")
+                    print("You dodged the bullet and brought peace between Man and Machine. You won the game you legend! The game will start over below\n")
 
 
         elif answer == "right":
