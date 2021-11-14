35e7b907863a900845f22352de678d8f4614f89a
robvclarke <robvclarke@gmail.com>
2021-11-14 15:14:52 +0000

    wrap main storyline in a function and call it

--- run.py
+++ run.py
@@ -1,56 +1,55 @@
-import pyfiglet
+def main_storyline():
+    while True:
+        print("Wake Up.\n")
+        print("The Matrix has you.\n")
+        print("Follow the white rabbit...\n")
+        print("""
+       /gg\           /gg\ 
+      /g.gg\         /gg.g\ 
+     |gg..gg\       /gg..gg| 
+     |gg...g|       |g...gg| 
+     |gg...g|       |g...gg| 
+      \gg..g/       \g..gg/ 
+       |gg.gvgggggggvg.gg| 
+      /ggggggggggggggggggg\ 
+     /gggg(((ggggggg)))gggg\ 
+    |ggggg....ggggg....ggggg| 
+    |ggggg....ggggg....ggggg| 
+    |ggcccgggg\___/ggggcccgg| 
+    |ggcccccgggg|ggggcccccgg| 
+      \gcccggg\---/gggcccg/ 
+         \ggggggggggggg/
+        """)
+        answer = input("Would you like to follow the white rabbit?(yes/no)\n")
 
-ascii_banner = pyfiglet.figlet_format("Wake Up Neo...")
+        if answer.lower().strip() == "yes":
 
-while True:
-    print("Wake Up Neo.\n")
-    print("The Matrix has you.\n")
-    print("Follow the white rabbit...\n")
-    print("""
-   /gg\           /gg\ 
-  /g.gg\         /gg.g\ 
- |gg..gg\       /gg..gg| 
- |gg...g|       |g...gg| 
- |gg...g|       |g...gg| 
-  \gg..g/       \g..gg/ 
-   |gg.gvgggggggvg.gg| 
-  /ggggggggggggggggggg\ 
- /gggg(((ggggggg)))gggg\ 
-|ggggg....ggggg....ggggg| 
-|ggggg....ggggg....ggggg| 
-|ggcccgggg\___/ggggcccgg| 
-|ggcccccgggg|ggggcccccgg| 
-  \gcccggg\---/gggcccg/ 
-     \ggggggggggggg/
-    """)
-    answer = input("Would you like to follow the white rabbit?(yes/no)\n")
+            answer = input("Would you like to follow it left or right?(left/right)\n").lower().strip()
+            if answer == "left":
+                answer = input("You encounter an agent, would you like to run or attack?\n")
 
-    if answer.lower().strip() == "yes":
-
-        answer = input("Would you like to follow it left or right?(left/right)\n").lower().strip()
-        if answer == "left":
-            answer = input("You encounter an agent, would you like to run or attack?\n")
-
-            if answer == "attack":
-                print("that was not the greatest idea, you lost")
-            else:
-                print("good choice you made it away")
-
-                answer = input("Another agent appears in the distance and fires a gun at you? Do you try and dodge it or run? (dodge/run)\n").lower().strip()
-
-                if answer == "run":
-                    print("You can't outrun a bullet... Game over\n")
+                if answer == "attack":
+                    print("that was not the greatest idea, you lost")
                 else:
-                    print("You dodged the bullet and brought peace between Man and Machine. You won the game you legend! The game will start over below\n")
+                    print("good choice you made it away")
+
+                    answer = input("Another agent appears in the distance and fires a gun at you? Do you try and dodge it or run? (dodge/run)\n").lower().strip()
+
+                    if answer == "run":
+                        print("You can't outrun a bullet... Game over\n")
+                    else:
+                        print("You dodged the bullet and brought peace between Man and Machine. You won the game you legend! The game will start over below\n")
 
 
-        elif answer == "right":
-            print("You chose the wrong hole. Game Over\n")
+            elif answer == "right":
+                print("You chose the wrong hole. Game Over\n")
 
-        else: 
-            print("invalid choice, you lost")
+            else: 
+                print("invalid choice, you lost")
 
 
-    else:
-        print("that's too bad")
-        break
+        else:
+            print("that's too bad")
+            break
+
+main_storyline()
