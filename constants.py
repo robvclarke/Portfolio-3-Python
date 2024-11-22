a6e781fa89c13e12f2683787b3d4fcfc2285c255
Robert Clarke <robvclarke@gmail.com>
2024-11-22 12:36:33 +0000

    Update constants so all classes have a name attribute

--- constants.py
+++ constants.py
@@ -1,8 +1,6 @@
 import random
 
 # class for green text
-
-
 class TerminalColors:
     OKGREEN = '\033[92m'
     HEADER = '\033[95m'
@@ -10,41 +8,36 @@ class TerminalColors:
     CRED = '\33[31m'
 
 # classes for characters you can choose to play as
-
-
 class Neo:
+    name = "Neo"  # Add name attribute
     health = 40
     strength = 11
     score = 0
 
-
 class Morpheus:
+    name = "Morpheus"  # Add name attribute
     health = 36
     strength = 9
     score = 0
 
-
 class Trinity:
+    name = "Trinity"  # Add name attribute
     health = 37
     strength = 7
     score = 0
 
 # classes for the enemies you will face
-
-
 class Agent:
     name = "Standard Agent"
     health = 20
     strength = 5
     loot = random.randint(0, 2)
 
-
 class Sentinel:
     name = "Sentinel"
     health = 15
     strength = 4
     loot = random.randint(0, 2)
-
 
 class Smith:
     name = "Agent Smith"
